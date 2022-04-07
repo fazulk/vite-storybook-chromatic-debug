63a9d49ac3f2c03fc7c6abeef58b883fbc98c5d2
fazulk <jfasulkey@gmail.com>
2022-04-07 09:52:25 -0700

    a

---
+++ src/assets/fontawesome-icons.ts
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronDown, faChevronRight, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faChevronDown, faChevronUp, faChevronRight)
+
+export default FontAwesomeIcon
