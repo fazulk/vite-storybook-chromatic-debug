2345c6f18b073666660bbb8f02a8dfdb9e4eb3bc
fazulk <jfasulkey@gmail.com>
2022-04-07 09:49:39 -0700

    try

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,27 @@
+const { resolve } = require("path");
+
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    interactionsDebugger: true,
+  },
   framework: "@storybook/vue3",
   core: {
     builder: "@storybook/builder-vite",
+  },
+  async viteFinal(config, { configType }) {
+    config.resolve.alias["@"] = resolve(__dirname, "../src");
+
+    return config;
   },
 };
--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
+  // eslint-disable-next-line
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_API_VERSION: string;
+  // more env variables...
+}
